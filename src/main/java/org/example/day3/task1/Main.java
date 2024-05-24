403c4409f9a5ec052abe7fc16d83781fcdef73e8
rabbie <rabbie@student.21-school.ru>
2024-05-24 14:23:40 +0300

    day3 - task1

---
+++ src/main/java/org/example/day3/task1/Main.java
@@ -0,0 +1,46 @@
+package org.example.day3.task1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] argc) {
+
+        Scanner scanner = new Scanner(System.in);
+        String[] strings = new String[3];
+
+        for (int i = 0; i < 3; i++)
+            strings[i] = scanner.next();
+
+        String shortestLine = findShortestString(strings);
+        System.out.println("Shortest line: " + "\"" + shortestLine + "\""
+                + ". Length: " + shortestLine.length());
+
+        String longestLine = findLongestString(strings);
+        System.out.println("Longest line: " + "\"" + longestLine + "\""
+                + ". Length: " + longestLine.length());
+    }
+
+    private static String findShortestString(String[] strings) {
+
+        String shortString = strings[0];
+
+        for (String string : strings) {
+            if (string.length() < shortString.length())
+                shortString = string;
+        }
+
+        return shortString;
+    }
+
+    private static String findLongestString(String[] strings) {
+
+        String shortString = strings[0];
+
+        for (String string : strings) {
+            if (string.length() > shortString.length())
+                shortString = string;
+        }
+
+        return shortString;
+    }
+}
